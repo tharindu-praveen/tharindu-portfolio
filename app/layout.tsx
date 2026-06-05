ffd322e0b7bd8188d09d83222d2bc424f2cca65c
Mr.Alpha <130214951+MrAplha@users.noreply.github.com>
2026-06-05 10:52:14 +0530

    Updated portfolio content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { ReactNode } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -80,10 +81,14 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <body>
         {children}
       </body>
     </html>
